e1374302c39697faae76db784d932ae4b9371223
kyma24 <kimi.y.ma24@gmail.com>
2025-10-27 22:51:06 -0500

    reset config

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
-const prod = process.env.NODE_ENV==="production";
-
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  images: {unoptimized:true},
-  ...(prod? {
-    basePath: "/algulus-line",
-    assetPrefix: "/algulus-line/",
-  } : {}),
 };
 
 export default nextConfig;
